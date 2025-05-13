72da0318ef570089b4236cfe0e54dc070df09850
Ken Tyler <ken@8thfold.com>
2025-05-12 17:36:56 -0700

    added build script for the server

--- app.js
+++ app.js
@@ -330,7 +330,7 @@ app.use((req, res, next) => {
 if (req.path.startsWith('/api/')) {
 return next();
 }
-res.sendFile(path.join(__dirname, 'react-app', 'index.html'));
+res.sendFile(path.join(__dirname, 'react-backstage/build', 'index.html'));
 });
 
 export default app;
